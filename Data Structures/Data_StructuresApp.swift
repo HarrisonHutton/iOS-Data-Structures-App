59f2bbc95ed8830c5916a520ee6a007a32a93de7
HarrisonHutton <harrison.hutton6@gmail.com>
2021-12-06 23:30:53 -0500

    Initial Commit

---
+++ Data Structures/Data_StructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Data_StructuresApp.swift
+//  Data Structures
+//
+//  Created by Harrison Hutton on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Data_StructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
